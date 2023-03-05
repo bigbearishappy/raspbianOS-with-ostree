9f3060622dda500347d3251c10bb37c5dd5e5f40
bigbearishappy <953308023@qq.com>
2023-03-05 19:21:48 +0800

    add steps to build u-boot if needed

--- 2-install_u-boot.sh
+++ 2-install_u-boot.sh
@@ -2,13 +2,27 @@
 
 echo build and install u-boot...
 
+if [ ! -f /host/u-boot.bin ]
+then
+apt -y build-dep u-boot
+echo u-boot.bin not exist, build it now ...
 cd /tmp
 
+git clone git://git.denx.de/u-boot.git --depth=1
+
+cd u-boot
+
+make CROSS_COMPILE=aarch64-linux-gnu- rpi_4_defconfig
+make -j`nproc`
+cp u-boot.bin /host
+fi
+
+cd /tmp
 cp /host/boot_cmd.txt ./
 mkimage -A arm64 -O linux -T script -C none -d boot_cmd.txt boot.scr
 
 # deploy the u-boot to target image
-cp u-boot/u-boot.bin $ROOTDIR/u-boot.bin
+cp /host/u-boot.bin $ROOTDIR/u-boot.bin
 cp boot.scr $ROOTDIR/boot.scr
 
 echo "enable_uart=1" >> $ROOTDIR/config.txt
