3c97901245f8b74229b2fd12a7c7b551fe38d15c
bigbearishappy <953308023@qq.com>
2023-03-05 19:28:45 +0800

    fix the entrypoint script

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,8 @@ export IMG=/tmp/$(basename $INPUT_IMG)
 export OUTPUT_IMG=/host/$(basename $INPUT_IMG .img)-ostree.img
 
 . /host/1-prepare_builddir.sh
-. /host/2-install_dracut_and_ostree.sh
-. /host/3-ostree_prep_rootfs.sh
-. /host/4-create_ostree_rootfs.sh
-. /host/5-cleanup.sh
+. /host/2-install_u-boot.sh
+. /host/3-install_dracut_and_ostree.sh
+. /host/4-ostree_prep_rootfs.sh
+. /host/5-create_ostree_rootfs.sh
+. /host/6-cleanup.sh
